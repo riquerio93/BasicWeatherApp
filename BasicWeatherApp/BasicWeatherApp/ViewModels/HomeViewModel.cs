722d6ddc5e3a6b0ca262b1131e2c8ef0e22dba5b
rorozco <riqueriver93@gmail.com>
2018-04-29 18:37:34 -0500

    * HomeView.xaml: * UserPrefs.xml: * HomeView.xaml.cs: * BasicWeatherApp.shproj: * BasicWeatherApp.projitems: * HomeViewModel.cs: test push update
    * map.cache:
    * libraryimports.cache:
    * BasicWeatherApp.iOS.exe:
    * BasicWeatherApp.iOS.pdb:
    * libraryprojectimports.cache:
    * abc_screen_toolbar.xml:
    * abc_action_menu_layout.xml:
    * avd_hide_password.xml:
    * avd_show_password.xml:
    * R.java:
    * design_bottom_sheet_dialog.xml:
    * abc_select_dialog_material.xml:
    * abc_action_mode_close_item_material.xml:
    * notification_media_cancel_action.xml:
    * design_appbar_state_list_animator.xml:

---
+++ BasicWeatherApp/BasicWeatherApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BasicWeatherApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+        }
+    }
+}
---
+++ BasicWeatherApp/BasicWeatherApp/Views/HomeView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+//using Xamarin.Forms;
+
+namespace BasicWeatherApp.Views
+{
+   /* public partial class HomeView : ContentPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    } */
+    
+    
+}
